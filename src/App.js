52d158c64c30b0bac2f2a8ed502b5927ed38d528
beblicarl <beblicarl.cb@gmail.com>
2021-10-11 12:27:13 +0000

    all features are functional now

--- src/App.js
+++ src/App.js
@@ -4,25 +4,62 @@ import "./App.css";
 import Questions from "./components/Questions";
 
 const API_URL =
-  "https://opentdb.com/api.php?amount=50&difficulty=easy&type=multiple";
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
 
 function App() {
   const [questions, setQuestions] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-
+  const [score, setScore] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        setQuestions(data.results);
+        const questions = data.results.map((question) => ({
+          ...question,
+          answers: [
+            question.correct_answer,
+            ...question.incorrect_answers,
+          ].sort(() => Math.random() - 0.5),
+        }));
+
+        setQuestions(questions);
       });
   }, []);
 
-  const handleAnswer = (answer) => {};
+  const handleAnswer = (answer) => {
+    // const newIndex = currentIndex + 1;
+    // setCurrentIndex(newIndex);
+
+    if (!showAnswers) {
+      if (answer === questions[currentIndex].correct_answer) {
+        setScore(score + 1);
+        console.log(score);
+      }
+    }
+    setShowAnswers(true);
+  };
+
+  const handleNextQuestion = () => {
+    setShowAnswers(false);
+    setCurrentIndex(currentIndex + 1);
+  };
 
   return questions.length > 0 ? (
     <div className="App">
-      <Questions data={questions[currentIndex]} handleAnswer={handleAnswer} />
+      {currentIndex >= questions.length ? (
+        <h1 className="text-3xl text-red-600 font-bold">
+          {" "}
+          Game Ended !! Your score is : {score}
+        </h1>
+      ) : (
+        <Questions
+          handleNextQuestion={handleNextQuestion}
+          showAnswers={showAnswers}
+          data={questions[currentIndex]}
+          handleAnswer={handleAnswer}
+        />
+      )}
     </div>
   ) : (
     <h2 className="text-2xl text-gray-900 font-bold">Loading...</h2>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,32 +1,42 @@
 const Questions = ({
+  handleNextQuestion,
+  showAnswers,
   handleAnswer,
-  data: { question, correct_answer, incorrect_answers },
+  data: { question, correct_answer, answers },
 }) => {
-  const shuffledAnswer = [correct_answer, ...incorrect_answers].sort(
-    () => Math.random() - 0.5
-  );
-  console.log(shuffledAnswer);
-
   return (
-    <div>
+    <div className="flex flex-col">
       {" "}
-      <div className="bg-gray-600 text-white p-4 rounded-lg shadow-md">
+      <div className="bg-indigo-400 text-white p-4 rounded-lg shadow-md">
         <h2
           className="text-2xl"
           dangerouslySetInnerHTML={{ __html: question }}
         />
       </div>
       <div className="grid grid-cols-2 gap-6 mt-6">
-        {shuffledAnswer.map((answer) => (
-          <button
-            className={`${
-              correct_answer === answer ? "bg-green-500" : " bg-gray-600"
-            }  p-4 font-semibold text-white rounded shadow `}
-            onClick={() => handleAnswer(answer)}>
-            {answer}
-          </button>
-        ))}
+        {answers.map((answer, idx) => {
+          const textColor = showAnswers
+            ? answer === correct_answer
+              ? "text-green-700"
+              : "text-red-700"
+            : "text-white";
+          return (
+            <button
+              key={idx}
+              className={`bg-indigo-400 p-4 font-semibold  rounded shadow ${textColor} `}
+              onClick={() => handleAnswer(answer)}
+              dangerouslySetInnerHTML={{ __html: answer }}
+            />
+          );
+        })}
       </div>
+      {showAnswers && (
+        <button
+          onClick={handleNextQuestion}
+          className={`bg-yellow-700 text-white text-xl p-4 font-semibold  rounded shadow mt-6`}>
+          Next Question
+        </button>
+      )}
     </div>
   );
 };
