1311088f58c4398e8dd42e57791cf300ff54dbd0
beblicarl <beblicarl.cb@gmail.com>
2021-11-23 19:58:57 +0000

    worked on the importation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Questions from "./components/Questions";
 
